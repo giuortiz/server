1552f99c128c3167e977746482c0abce9d0b8759
giuortiz <50875077+giuortiz@users.noreply.github.com>
2019-10-08 14:36:27 -0300

    Update Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,11 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
+using FirebaseSharp.Portable;
 using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
+using Newtonsoft.Json;
 
 
 namespace Server
@@ -25,6 +28,9 @@ namespace Server
     {
         public string Nome { get; set; }
         public int nUsp { get; set; }
+        public bool pertenceGrupo { get; set; }
+
+
     }
     class Program
     {
@@ -42,14 +48,24 @@ namespace Server
             Grupo RegisteredUsers = new Grupo();
             RegisteredUsers.Integrantes = new List<Pessoa>();
 
-            //  async void insereTeste()
-            // {
-            //     var Pessoa = new Pessoa() { nUsp = 9911430, Nome = "Gabriel Folegatti Santana" };
-            //     SetResponse response = await cliente.SetTaskAsync("Pessoa/" + Pessoa.Nome,Pessoa );
-            //     Pessoa result = response.ResultAs<Pessoa>();
-            //     Console.WriteLine(result.Nome);
-            //  }
-            // insereTeste();
+            //insereTeste();
+            List<Pessoa> estudantes = new List<Pessoa>();
+            estudantes.Add(new Pessoa() { nUsp = 9878079, Nome = "Giuliano Ortiz Goria", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 991490, Nome = "Nicole Wilcox", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9911520, Nome = "Adrian Martinson", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9911455, Nome = "Nora Osborn", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9878079, Nome = "Indio Zellar", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 991490, Nome = "Victor Zellar", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9911520, Nome = "Monstruosidade Monstruosa", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9911455, Nome = "Enzo Zellar", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9878079, Nome = "Jairo Justus", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 991490, Nome = "Como Estamos?", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9911520, Nome = "Spider Silva", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9911455, Nome = "jorginho Doze", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9878079, Nome = "Ingrid Gabrielly", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 991490, Nome = "Olaff Cornin", pertenceGrupo = false });
+            estudantes.Add(new Pessoa() { nUsp = 9911455, Nome = "Psyduck da Silva", pertenceGrupo = false });
+
 
             //   RegisteredUsers.Tema = "Idade Média";
             RegisteredUsers.Integrantes.Add(new Pessoa() { nUsp = 9878079, Nome = "Giuliano Ortiz Goria" });
@@ -92,49 +108,99 @@ namespace Server
             grupos.Add(RegisteredUsers4);
             grupos.Add(RegisteredUsers5);
 
-        void servidor() {
+            void servidor()
+            {
+
                 TcpListener server = new TcpListener(IPAddress.Any, 9999);
+                Console.WriteLine("Iniciando servidor...");
+
                 // we set our IP address as server's address, and we also set the port: 9999
-                server.Start();  // this will start the serve
-                Console.WriteLine("Esperando algum doido...");
+                try {
+                    server.Start(); 
+                } catch {
+                    servidor();
+                }
+                Console.WriteLine("Servidor iniciado...");
+                // this will start the serve
+                Console.WriteLine("Esperando alguma conexão...");
                 while (true)   //we wait for a connection
                 {
                     TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
-                    Console.WriteLine("Doido chegou...");
+                    Console.WriteLine("conexão estabelecida...");
                     NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages//sending the message
                     string clientMessage = string.Empty;
                     byte[] serverMessage = new byte[100];
                     while (client.Connected)  //while the client is connected, we look for incoming messages
                     {
                         byte[] msg = new byte[1024];     //the messages arrive as byte array
-                        ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+                        try
+                        {
+                            ns.Read(msg, 0, msg.Length);
+                        }
+                        catch
+                        {
+                            servidor();
+                        }  //the same networkstream reads the message sent by the client
                         clientMessage = Encoding.UTF8.GetString(msg).Replace("\0", "");
 
                         //now , we write the message as string
-                        var p = new JavaScriptSerializer().Deserialize<Pessoa>(clientMessage);
-                        if (p != null)
+                        try
                         {
-                            if (p.Nome == "Xolis")
+                            var p = new JavaScriptSerializer().Deserialize<Pessoa>(clientMessage);
+                            if (p != null)
                             {
-                                var serializer = new JavaScriptSerializer();
-                                var serializedResult = serializer.Serialize(grupos);
-                                serverMessage = Encoding.Default.GetBytes(serializedResult);
+                                if (p.Nome == "Xolis")
+                                {
 
-                            }
-                            else
-                            {
-                                serverMessage = Encoding.Default.GetBytes("a");
+                                    Console.WriteLine("Enviando lista de Grupos...");
+
+                                    var serializer = new JavaScriptSerializer();
+                                    var serializedResult = serializer.Serialize(grupos);
+                                    serverMessage = Encoding.Default.GetBytes(serializedResult);
+
+                                }
+                                else if (p.Nome == "foles")
+                                {
+                                    Console.WriteLine("Enviando lista de alunos...");
+
+                                    var serializer = new JavaScriptSerializer();
+                                    var serializedResult = serializer.Serialize(estudantes);
+                                    serverMessage = Encoding.Default.GetBytes(serializedResult);
+                                }
+                                else
+                                {
+                                    var s = new JavaScriptSerializer().Deserialize<Grupo>(clientMessage);
+                                    if (s != null)
+                                    {
+                                        grupos.Add(s);
+                                        Console.WriteLine("Lista Grupo Adicionado...");
+                                        Console.WriteLine(s.Integrantes[0].Nome);
+                                    }
+                                }
                             }
                         }
-                        try { ns.Write(serverMessage, 0, serverMessage.Length); } catch  {
+                        catch
+                        {
+
+
+                        }
+
+
+                        try
+                        {
+                            ns.Write(serverMessage, 0, serverMessage.Length);
+                            Console.WriteLine("Lista enviada...");
+
+                        }
+                        catch
+                        {
                             server.Stop();
                             servidor();
+                            Console.WriteLine("Perdi...");
+
                         }
-                       
-                        if (p != null)
-                        {
-                            Console.WriteLine($"{p.Nome}");
-                        }
+
+
                     }
                 }
                 servidor();
